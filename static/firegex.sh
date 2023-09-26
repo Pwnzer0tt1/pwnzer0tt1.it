2ca52f85737fddfc12f42786462df5758d9adcca
Domingo Dirutigliano <me@domy.sh>
2023-09-26 23:53:38 +0200

    Update firegex.sh

--- static/firegex.sh
+++ static/firegex.sh
@@ -1,3 +1,8 @@
 #/usr/bin/env bash
-curl -sLf https://raw.githubusercontent.com/Pwnzer0tt1/firegex/main/start.py > firegex.py
-exec python3 firegex.py "$@"
+FILE=firegex.py
+if [ -f "$FILE" ]; then
+        echo "Cannot create '$FILE'... file already exists!";
+        exit 1;
+fi
+curl -sLf https://raw.githubusercontent.com/Pwnzer0tt1/firegex/main/start.py > $FILE
+exec python3 $FILE "$@"
