7ceefd07a3316888e5c382c63c21fb3d593fbc86
Domingo Dirutigliano <me@domy.sh>
2025-04-05 13:06:41 +0200

    Update xfarm.sh

--- static/xfarm.sh
+++ static/xfarm.sh
@@ -4,5 +4,17 @@ if [ -f "$FILE" ]; then
         echo "Cannot create '$FILE'... file already exists!";
         exit 1;
 fi
+
+which docker &> /dev/null || (echo "Install docker before running this command!"; exit 1)
+docker ps 2>&1 | grep "daemon running" &> /dev/null && (echo "Docker is not running! Please start docker daemon before use this command!"; exit 1)
+
+USE_SUDO=0
+docker ps 2>&1 | grep "permission denied" &> /dev/null && USE_SUDO=1
+
 curl -sLf https://raw.githubusercontent.com/Pwnzer0tt1/exploitfarm/main/start.py > $FILE
-exec python3 $FILE "$@"
+
+if [[ "$USE_SUDO" == "1" ]]; then
+  exec sudo python3 $FILE "$@"
+else
+  exec python3 $FILE "$@"
+fi
