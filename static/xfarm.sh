7dda74fa578c713d081f02dbc65b722691db042b
Domingo Dirutigliano <me@domy.sh>
2025-05-16 15:15:53 +0200

    Update xfarm.sh

--- static/xfarm.sh
+++ static/xfarm.sh
@@ -1,20 +1,22 @@
-#/usr/bin/env bash
+#!/bin/sh
 FILE=exploitfarm.py
+
 if [ -f "$FILE" ]; then
-        echo "Cannot create '$FILE'... file already exists!";
-        exit 1;
+        echo "Cannot create '$FILE'... file already exists!"
+        exit 1
 fi
 
-which docker &> /dev/null || (echo "Install docker before running this command!"; exit 1)
-docker ps 2>&1 | grep "daemon running" &> /dev/null && (echo "Docker is not running! Please start docker daemon before use this command!"; exit 1)
+which docker > /dev/null 2>&1 || { echo "Install docker before running this command!"; exit 1; }
+
+docker ps 2>&1 | grep "daemon running" > /dev/null && { echo "Docker is not running! Please start docker daemon before use this command!"; exit 1; }
 
 USE_SUDO=0
-docker ps 2>&1 | grep "permission denied" &> /dev/null && USE_SUDO=1
+docker ps 2>&1 | grep "permission denied" > /dev/null && USE_SUDO=1
 
-curl -sLf https://raw.githubusercontent.com/Pwnzer0tt1/exploitfarm/main/start.py > $FILE
+curl -sLf https://raw.githubusercontent.com/Pwnzer0tt1/exploitfarm/main/start.py > "$FILE"
 
-if [[ "$USE_SUDO" == "1" ]]; then
-  exec sudo python3 $FILE "$@"
+if [ "$USE_SUDO" -eq "1" ]; then
+  exec sudo python3 "$FILE" "$@"
 else
-  exec python3 $FILE "$@"
+  exec python3 "$FILE" "$@"
 fi
